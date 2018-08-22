2825be786b87d0debdfe65abc30b5d2a491afc6f
Wojciech <golewski.wojciech@outlook.com>
2018-08-22 20:19:02 +0200

    Added HelloWorldController class to test out Controllers

---
+++ MVC_WebApp_ATWD/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVC_WebApp_ATWD.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is a default action.";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method.";
+        }
+        
+    }
+}
