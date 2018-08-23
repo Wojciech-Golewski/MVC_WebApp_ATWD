dcf88998d09c6028538ccd5cd3732dca341156eb
Wojciech <golewski.wojciech@outlook.com>
2018-08-23 18:22:40 +0200

    added Rating variable to MVC

--- MVC_WebApp_ATWD/Models/Movie.cs
+++ MVC_WebApp_ATWD/Models/Movie.cs
@@ -10,12 +10,13 @@ namespace MVC_WebApp_ATWD.Models
         public string Title { get; set; }
 
         [Display(Name = "Released Date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
 
         [Column(TypeName = "decimal(18, 2")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
