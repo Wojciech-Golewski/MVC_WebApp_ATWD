3eb79cb407369c092064f0e0e2171d9a35395d3f
Wojciech <golewski.wojciech@outlook.com>
2018-08-23 16:49:25 +0200

    updated Model of Movie.cs

--- MVC_WebApp_ATWD/Models/Movie.cs
+++ MVC_WebApp_ATWD/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVC_WebApp_ATWD.Models
 {
@@ -7,9 +8,14 @@ namespace MVC_WebApp_ATWD.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Released Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2")]
         public decimal Price { get; set; }
     }
 }
